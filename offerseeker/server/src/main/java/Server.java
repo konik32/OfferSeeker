cf958d1933586bd16cf28c329990279575bf4f90
bambalooon <k.bb@wp.pl>
2014-10-21 21:01:28 +0200

    CM# server fix

---
+++ offerseeker/server/src/main/java/Server.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Krzysztof Balon on 2014-10-21.
+ */
+public class Server {
+}
---
+++ offerseeker/server/src/main/java/pl/edu/agh/offerseeker/Server.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.offerseeker;
+
+/**
+ * Created by Krzysztof Balon on 2014-10-21.
+ */
+public class Server {
+}
