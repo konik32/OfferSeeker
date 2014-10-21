59bbfaeddea6e4cf349b975d1945fb27045352db
szymon <konik32@gmail.com>
2014-10-22 00:39:50 +0200

    Server packaging changed to jar, added spring-boot-configuration with jetty, hibernate, mysql, spring-data-jpa

--- server/src/main/java/pl/edu/agh/offerseeker/Server.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.offerseeker;
-
-/**
- * Created by Krzysztof Balon on 2014-10-21.
- */
-public class Server {
-}
--- server/src/test/java/ServerTest.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Krzysztof Balon on 2014-10-21.
- */
-public class ServerTest {
-}
