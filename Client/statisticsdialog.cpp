0bed2b013b75dca270b4c689333f8e8817cb134f
Munez3 <melwin03@gmail.com>
2014-11-17 18:18:34 +0100

    Date in StatisticWindow
    Added current date to statisticwindow

--- Client/statisticsdialog.cpp
+++ Client/statisticsdialog.cpp
@@ -6,9 +6,37 @@ StatisticsDialog::StatisticsDialog(QWidget *parent) :
     ui(new Ui::StatisticsDialog)
 {
     ui->setupUi(this);
+    setCurrentDate();
+    ui->startdateEdit->setEnabled(false);
+    ui->enddateEdit->setEnabled(false);
 }
 
 StatisticsDialog::~StatisticsDialog()
 {
     delete ui;
+}
+
+void StatisticsDialog::setCurrentDate(){
+    QDate date = QDate::currentDate();
+    ui->startdateEdit->setDate(date);
+    ui->enddateEdit->setDate(date);
+}
+
+void StatisticsDialog::on_startDate_cbox_clicked(){
+    if(ui->startDate_cbox->isChecked()){
+        ui->startdateEdit->setEnabled(true);
+    }
+    else{
+        ui->startdateEdit->setEnabled(false);
+    }
+}
+
+
+void StatisticsDialog::on_endDate_cbox_clicked(){
+    if(ui->endDate_cbox->isChecked()){
+        ui->enddateEdit->setEnabled(true);
+    }
+    else{
+        ui->enddateEdit->setEnabled(false);
+    }
 }
--- Client/statisticsdialog.h
+++ Client/statisticsdialog.h
@@ -2,6 +2,7 @@
 #define STATISTICSDIALOG_H
 
 #include <QDialog>
+#include <QDate>
 
 namespace Ui {
 class StatisticsDialog;
@@ -11,12 +12,19 @@ class StatisticsDialog : public QDialog
 {
     Q_OBJECT
 
+private slots:
+    void on_startDate_cbox_clicked();
+    void on_endDate_cbox_clicked();
+
 public:
     explicit StatisticsDialog(QWidget *parent = 0);
     ~StatisticsDialog();
 
 private:
     Ui::StatisticsDialog *ui;
+    void setCurrentDate();
 };
+
+
 
 #endif // STATISTICSDIALOG_H
